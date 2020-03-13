82a71bbc5361bbd377c698f7a6c3370a778f0f2c
Gary Larson <gary.larson@larsonapps.com>
2020-03-13 18:39:34 -0500

    Team B Layout Complete

--- app/src/main/java/com/example/android/americanfootballcounter/MainActivity.java
+++ app/src/main/java/com/example/android/americanfootballcounter/MainActivity.java
@@ -7,8 +7,9 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    // Declare and initialize Team A's score
+    // Declare and initialize Team scores
     private int scoreTeamA = 0;
+    private int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +17,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     *  This method displays the points on the screen for Team A
+     * @param points - points to display
+     */
     private void displayPointsTeamA (int points) {
         TextView view = findViewById(R.id.score_teamA_text_view);
+        view.setText(getApplicationContext().getString(R.string.number, points));
+    }
+
+    /**
+     *  This method displays the points on the screen for Team B
+     * @param points - points to display
+     */
+    private void displayPointsTeamB (int points) {
+        TextView view = findViewById(R.id.score_teamB_text_view);
         view.setText(getApplicationContext().getString(R.string.number, points));
     }
 
@@ -56,4 +70,41 @@ public class MainActivity extends AppCompatActivity {
         scoreTeamA ++;
         displayPointsTeamA(scoreTeamA);
     }
+
+    /**
+     * This method adds 6 points to Team B's score For Touch Downs
+     * @param view - called by onClick
+     */
+    public void addSixPointsTeamB(View view) {
+        scoreTeamB += 6;
+        displayPointsTeamB(scoreTeamB);
+    }
+
+    /**
+     * This method adds 3 points to Team B's score for Field Goals
+     * @param view - called by onClick
+     */
+    public void addThreePointsTeamB(View view) {
+        scoreTeamB += 3;
+        displayPointsTeamB(scoreTeamB);
+    }
+
+    /**
+     * This method adds 2 points to Team B's score for Two point extra points and Safety
+     * @param view - called by onClick
+     */
+    public void addTwoPointsTeamB(View view) {
+        scoreTeamB += 2;
+        displayPointsTeamB(scoreTeamB);
+    }
+
+    /**
+     * This method adds 1 points to Team B's score for extra point
+     * @param view - called by onClick
+     */
+    public void addExtraPointTeamB(View view) {
+        scoreTeamB ++;
+        displayPointsTeamB(scoreTeamB);
+    }
+
 }
